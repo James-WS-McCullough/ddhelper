df366c9c4565dba3c499ecf6054b9cf8f247959c
James McCullough <james.mccullough@aaoglobal.com>
2023-08-23 23:09:33 +0100

    feat: fadeout and fixes

--- src/pages/FileDropper.tsx
+++ src/pages/FileDropper.tsx
@@ -6,32 +6,72 @@ const AudioPlayer = ({ fileObject }) => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(50);
   const [audio, setAudio] = useState(null);
+  const FADE_DURATION = 2000;
+  const STEPS = 40;
+  const stepDuration = FADE_DURATION / STEPS;
+  const [decrementStep, setDecrementStep] = useState(volume / 100 / STEPS);
+
 
   useEffect(() => {
     const audioInstance = new Audio(src);
     setAudio(audioInstance);
-
-    // Clean up the audio instance when the component is unmounted
-    return () => {
-      audioInstance.pause();
-      setAudio(null);
+    
+    audioInstance.onended = () => {
+      setIsPlaying(false);
     };
-  }, [src]);
 
-  const handlePlay = () => {
-    setIsPlaying(true);
-    audio.play();
+    if (fileObject.loop) {
+      audioInstance.addEventListener('ended', () => {
+        audioInstance.currentTime = 0;
+        audioInstance.play();
+      });
+    }
+
+    return () => {
+      audioInstance.pause(); // Ensure audio is paused when the component is unmounted
+      audioInstance.removeEventListener('ended', audioInstance.onended);
+    };
+  }, [src, fileObject.loop]);
+
+  const fadeOut = () => {
+    if (!audio) return; // Guard clause
+
+    if (audio.volume > decrementStep) {
+      audio.volume -= decrementStep;
+      setTimeout(fadeOut, stepDuration);
+    } else {
+      audio.volume = 0;
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+      setIsPlaying(false);
+    }
   };
 
-  const handlePause = () => {
-    setIsPlaying(false);
-    audio.pause();
+  const handleToggle = () => {
+    if (!audio) return; // Guard clause
+
+    if (isPlaying) {
+      if (fileObject.loop) {
+        fadeOut();
+      } else {
+        audio.pause();
+        audio.currentTime = 0;
+        setIsPlaying(false);
+      }
+    } else {
+      audio.volume = volume / 100; // Ensure we reset the volume when playing again
+      audio.play();
+      setIsPlaying(true);
+    }
   };
 
   const handleVolumeChange = (value) => {
     setVolume(value);
     if (audio) {
       audio.volume = value / 100;
+      setDecrementStep(audio.volume / STEPS); // Update decrement step when volume changes
     }
   };
 
@@ -55,10 +95,13 @@ const AudioPlayer = ({ fileObject }) => {
       <VStack w="25%" spacing={4}> {/* Remaining space for the button. Adjust width if needed */}
       </VStack>
       {isPlaying ? (
-        <Button onClick={handlePause}>Pause</Button>
-      ) : (
-        <Button onClick={handlePlay}>Play</Button>
-      )}
+  <Button onClick={handleToggle}>
+    {fileObject.loop ? "Fade" : "Stop"}
+  </Button>
+) : (
+  <Button onClick={handleToggle}>Play</Button>
+)}
+
     </HStack>
   );
 };
